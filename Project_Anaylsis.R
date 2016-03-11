429c7dbf176e73485f2f2c62052ba5fd1ba4daa0
Jasondang <jasondang@live.com.au>
2016-03-11 15:40:50 +1100

    Update Project_Anaylsis.R

--- Project_Anaylsis.R
+++ Project_Anaylsis.R
@@ -61,5 +61,5 @@ names(Data) <- gsub("BodyBody", "Body", names(Data))
 meltData <- melt(Data, id = c("Subject", "Activity"))
 averageData <- dcast(meltData, Subject + Activity ~ variable, mean)
 
-write.table(averageData, "tidy_data_2.txt", row_name=FALSE)
+write.table(averageData, "tidy_data_2.txt", row.name=FALSE)
 
